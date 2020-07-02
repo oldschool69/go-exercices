9ac8b15e79f2fd6355170ae68a5fb7c301c3d25b
oldschool69 <flavio.marcondes@gmail.com>
2020-07-02 14:40:14 -0300

    sample using test framework

---
+++ 12-001/dog/main.go
@@ -0,0 +1,7 @@
+// package for human to dog conversion
+package dog
+
+// This functions convert human's life years to dog years
+func Years(y int) int {
+	return y * 7
+}
---
+++ 12-001/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"go-exercises/12-001/dog"
+)
+
+func main() {
+
+	dy := dog.Years(2)
+	fmt.Println("2 human years in dog years is: ", dy)
+
+}
